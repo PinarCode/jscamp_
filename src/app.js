aac792d5ad4f2cc53556819cc81f238db08f7d30
PinarCode <kayapinar4706@gmail.com>
2021-10-21 09:36:46 +0300

    deneme

--- src/app.js
+++ src/app.js
@@ -20,7 +20,6 @@ for(let i = 0; i<konutKredileri.length; i++){
     console.log("<li>"+konutKredileri[i]+"<li>")
 }
 console.log("</ul>")
-
 {/* <ul>
     <li>Konut kredisi</li>
     <li>Emlak Konut kredisi</li>
